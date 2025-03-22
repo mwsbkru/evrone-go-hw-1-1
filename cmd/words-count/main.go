a841f572844cde9ca392f8014dd203894965d8dd
M.Sychev <ext-mikhail.sychev@sbermarket.ru>
2025-03-22 22:14:23 +0300

    first commit

---
+++ cmd/words-count/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var wordNormalizeRegex = regexp.MustCompile(`[^A-Za-zА-Яа-я]`)
+
+type wordEntry struct {
+	word  string
+	count int
+}
+
+func main() {
+	var resultCount int
+	flag.IntVar(&resultCount, "n", 10, "Сколько наиболее используемых слов отображать")
+	flag.Parse()
+	fileName := flag.Arg(0)
+
+	if fileName == "" {
+		fmt.Println("Не указано имя файла для открытия")
+	}
+
+	wordsUsageRaw, err := readWordsFromFile(fileName)
+	if err != nil {
+		fmt.Println("Ошибка при открытии файла: ", err)
+		return
+	}
+
+	sortedWordEntries := sortWordEntries(wordsUsageRaw)
+
+	if resultCount > len(sortedWordEntries) {
+		resultCount = len(sortedWordEntries)
+	}
+
+	for i := 0; i < resultCount; i++ {
+		fmt.Printf("%v: %d\n", sortedWordEntries[i].word, sortedWordEntries[i].count)
+	}
+}
+
+func readWordsFromFile(fileName string) (map[string]int, error) {
+	wordsUsage := make(map[string]int)
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		word := normalizeWord(scanner.Text())
+		if word == "" {
+			continue
+		}
+
+		wordsUsage[word] += 1
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return wordsUsage, nil
+}
+
+func sortWordEntries(wordsUsage map[string]int) []wordEntry {
+	sliceForSort := make([]wordEntry, 0, len(wordsUsage))
+
+	for word, count := range wordsUsage {
+		sliceForSort = append(sliceForSort, wordEntry{word: word, count: count})
+	}
+
+	sort.Slice(sliceForSort, func(i, j int) bool {
+		return sliceForSort[i].count > sliceForSort[j].count
+	})
+
+	return sliceForSort
+}
+
+func normalizeWord(word string) string {
+	word = wordNormalizeRegex.ReplaceAllString(word, "")
+	word = strings.ToLower(word)
+	return word
+}
